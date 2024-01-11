e700e30d0ca262e20af8a6509100e0a531665cb6
ZoontS <89505339+ZoontS@users.noreply.github.com>
2024-01-12 01:24:33 +0700

    Change Target Video Size to 25 MB
    Other changes include:
    
    - Using bundled ffmpeg if exists
    
    - Add number suffix if video of the same name already exists
    
    - Use bitrate instead of duration for categorizing videos. This is done so that if target file size is changed the rest of the code don't need to be changed
    
    - Tweaked some resolution and bitrate cutoffs

--- Video2Discord.py
+++ Video2Discord.py
@@ -5,8 +5,16 @@
 import os, time, json, winsound, shutil, sys
 from tkinter import filedialog
 
-if str(shutil.which("ffmpeg")) == "None":
-    print("FFmpeg is not installed in your computer. Please download FFmpeg and add it to PATH. If you need a guide on how to install it, you can watch a tutorial like this one: \033]8;;https://youtu.be/IECI72XEox0\033\\https://youtu.be/IECI72XEox0\033]8;;\033\\")
+ffmpeg = "ffmpeg"
+ffprobe = "ffprobe"
+
+if os.path.isfile("FFmpeg/bin/ffmpeg.exe") == True and os.path.isfile("FFmpeg/bin/ffprobe.exe") == True:
+    print("using bundled FFmpeg")
+    ffmpeg = "FFmpeg\\bin\\ffmpeg.exe"
+    ffprobe = "FFmpeg\\bin\\ffprobe.exe"
+
+if (str(shutil.which("ffmpeg")) == "None" and os.path.isfile("FFmpeg/bin/ffmpeg.exe") == False) or (str(shutil.which("ffprobe")) == "None" and os.path.isfile("FFmpeg/bin/ffprobe.exe") == False):
+    print("FFmpeg is not installed in your computer. Please download FFmpeg and add it to PATH. If you need a guide on how to install it, you can watch a tutorial like this one: https://youtu.be/IECI72XEox0")
     time.sleep(1)
     input("Press the enter key to exit ")
     sys.exit()
@@ -20,7 +28,13 @@ for input_video in input_video_list:
     output_video = os.path.basename(input_video)
     output_video = ".".join(output_video.split(".")[:-1])
 
-    os.system(f"ffprobe -hide_banner -of json -select_streams v -show_format -show_streams -i \"{input_video}\" -o \"temp.json\"")
+    if os.path.isfile(f"Videos/{output_video}.webm") == True:
+        suffix = 1
+        while os.path.isfile(f"Videos/{output_video} ({suffix}).webm") == True:
+            suffix += 1
+        output_video = f"{output_video} ({suffix})"
+
+    os.system(f"{ffprobe} -hide_banner -of json -select_streams v -show_format -show_streams -i \"{input_video}\" -o \"temp.json\"")
 
     with open("temp.json", "r") as openfile:
         video_data = json.load(openfile)
@@ -28,7 +42,11 @@ for input_video in input_video_list:
     duration = float(video_data["format"]["duration"])
     width = int(video_data["streams"][0]["width"])
     height = int(video_data["streams"][0]["height"])
-    video_bitrate = (65536 / duration) * 0.99
+    video_bitrate = (204800 / duration)
+    if video_bitrate >= 1000:
+        video_bitrate = video_bitrate * 0.99
+    if video_bitrate < 1000:
+        video_bitrate = video_bitrate * 0.98
 
     if width >= height:
         video_res = "-vf scale=-1:1080"
@@ -38,31 +56,39 @@ for input_video in input_video_list:
     audio_channel = ""
     audio_bitrate = 128
 
-    if duration >= 31:
+    if video_bitrate > 6000:
+        video_bitrate = 6000
+
+    if video_bitrate < 3000:
         video_res = video_res.replace("1080", "720")
         audio_bitrate = 96
 
-    if duration >= 61:
+    if video_bitrate < 1500:
         video_res = video_res.replace("720", "540")
         audio_bitrate = 64
 
-    if duration >= 91:
-        video_res = video_res.replace("540", "480")
+    if video_bitrate < 1000:
         video_fps = 30
 
-    if duration >= 121:
+    if video_bitrate < 750:
+        video_res = video_res.replace("540", "480")
+
+    if video_bitrate < 564:
         audio_bitrate = 48
 
-    if duration >= 181:
-        video_res = video_res.replace("480", "320")
+    if video_bitrate < 448:
+        video_res = video_res.replace("480", "360")
+        
+    if video_bitrate < 348:
+        video_res = video_res.replace("360", "240")
         audio_bitrate = 32
 
-    if duration >= 301:
-        video_res = video_res.replace("320", "240")
+    if video_bitrate < 232:
         audio_bitrate = 24
         audio_channel = "-ac 1 -apply_phase_inv 0"
 
-    if duration >= 421:
+    if video_bitrate < 160:
+        video_res = video_res.replace("240", "144")
         audio_bitrate = 14
 
     if width >= height:
@@ -72,7 +98,7 @@ for input_video in input_video_list:
         if width <= int(video_res.replace("-vf scale=", "").replace(":-1", "")):
             video_res = ""
 
-    os.system(f"ffmpeg -hide_banner -loglevel warning -stats -i \"{input_video}\" -fpsmax {video_fps} {video_res} -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -b:v {video_bitrate - audio_bitrate}k -pass 1 -an -y -f null NUL && ^ffmpeg -hide_banner -loglevel warning -stats -i \"{input_video}\" -fpsmax {video_fps} {video_res} -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -b:v {video_bitrate - audio_bitrate}k -pass 2 -c:a libopus {audio_channel} -b:a {audio_bitrate}k \"Videos/{output_video}.webm\"")
+    os.system(f"{ffmpeg} -hide_banner -loglevel warning -stats -i \"{input_video}\" -fpsmax {video_fps} {video_res} -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -b:v {video_bitrate - audio_bitrate}k -pass 1 -an -y -f null NUL && ^{ffmpeg} -hide_banner -loglevel warning -stats -i \"{input_video}\" -fpsmax {video_fps} {video_res} -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -b:v {video_bitrate - audio_bitrate}k -pass 2 -c:a libopus {audio_channel} -b:a {audio_bitrate}k \"Videos/{output_video}.webm\"")
     
     print("")
     if os.path.isfile("temp.json") == True:
@@ -80,6 +106,6 @@ for input_video in input_video_list:
     if os.path.isfile("ffmpeg2pass-0.log") == True:
         os.remove("ffmpeg2pass-0.log")
 
-winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
-print("Finished transcoding all videos")
-time.sleep(1)
+if input_video_list:
+    winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
+    time.sleep(1)
